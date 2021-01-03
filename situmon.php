530cdc6f4fe37612abe2988f400b1591f597af4d
kishikonomaid <17e1143andrew.ac.jp>
2021-01-03 21:44:08 +0900

    hekou

--- situmon.php
+++ situmon.php
@@ -5,15 +5,35 @@ require_once('db_connect.php');
 
 $pdo = get_pdo();
 if (
+    isset($_POST['タイトル'])
+
+) {
+    $statement = $pdo->prepare(
+        "INSERT INTO アンケート一覧 (タイトル) VALUES (:title);"
+    );
+    $statement->execute([
+        ':title' => $_POST['タイトル'],
+        
+    ]);
+}
+
+$data = $pdo->prepare('SELECT * FROM アンケート一覧');
+$data->execute();
+$gakusei = $data->fetchAll();
+
+$pdo = get_pdo();
+if (
     isset($_POST['質問1'])
 
 ) {
     $statement = $pdo->prepare(
-        "INSERT INTO 質問一覧 (質問1,) VALUES (:質問1,);"
+        "INSERT INTO 質問一覧 (質問文) VALUES (:question1), (:question2), (:question3), (:question4);"
     );
     $statement->execute([
-        ':質問1' => $_POST['質問1'],
-        
+        ':question1' => $_POST['質問1'],
+        ':question2' => $_POST['質問2'],
+        ':question3' => $_POST['質問3'],
+        ':question4' => $_POST['質問4'],
     ]);
 }
 
@@ -27,12 +47,30 @@ $gakusei = $data->fetchAll();
         <ul>
         <?php foreach ($gakusei as $row): ?>
             <li>
-                <?php echo htmlspecialchars($row['質問1']); ?>
+                <?php echo nl2br(htmlspecialchars($row['タイトル'])); ?>
+                <?php echo nl2br(htmlspecialchars($row['質問文'])); ?>
             </li>
         <?php endforeach; ?>
         </ul>
-        <form action="sandbox.digiroid.org" method="post">
-            <input type="search" name="質問1" value="">
+        <form action="situmonn.php" method="post">
+        <p>タイトル
+　　　　<input type="search" name="タイトル" value=""><br>
+        </p>
+          質問1
+        <textarea name="質問1"></textarea>
+        </p>
+        <p>
+          質問2
+            <input type="search" name="質問2" value=""><br>
+        </p>
+        <p>
+          質問3
+            <input type="search" name="質問3" value=""><br>
+        </p>
+        <p>
+          質問4
+            <input type="search" name="質問4" value="">
+        </p>
             <input type="submit" name="submit" value="追加">
         </form>
     </body>
