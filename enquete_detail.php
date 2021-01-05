5cb4adaa7387b7ba9dfa0ee859fb992188196278
kishikonomaid <17e1143andrew.ac.jp>
2021-01-05 14:36:34 +0900

    送信

--- enquete_detail.php
+++ enquete_detail.php
@@ -10,35 +10,7 @@ $statement = $pdo->prepare(
     ON アンケート一覧.ID = situmonnichirann.アンケートID
     WHERE アンケート一覧.ID = :enquete_id
     ');
- if (
-    isset($_POST["解答1"])
-) {
-  // アンケート一覧に挿入したレコードのIDを取得
-  $statement = $pdo->query('SELECT LAST_INSERT_ID() AS ID FROM situmonnichirann LIMIT 1;');
-  $situmonn_id = $statement->fetch()['ID'];
-  var_dump($situmonn_id);
 
-  $statement = $pdo->prepare(
-    "INSERT INTO answer (解答内容, 質問)
-    VALUES
-    (:answer1, :situmonn1),
-    (:answer2, :situmonn2),
-    (:answer3, :situmonn3),
-    (:answer4, :situmonn4);"
-    );
-    $statement->execute([
-        ':answer1' => $_POST["解答1"],
-        ':answer2' => $_POST['解答2'],
-        ':answer3' => $_POST['解答3'],
-        ':answer4' => $_POST['解答4'],
-        ':situmonn1' => $situmonn_id,
-        ':situmonn2' => $situmonn_id,
-        ':situmonn3' => $situmonn_id,
-        ':situmonn4' => $situmonn_id,
-    ]);
-}   
-$statement->execute([':situmonn_id' => $situmonn_id]);
-$answer_data = $statement->fetchAll();
 
 $statement->execute([':enquete_id' => $enquete_id]);
 $enquete_data = $statement->fetchAll();
